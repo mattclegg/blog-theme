844ff4a52028c17e01f94cad4746c1e6c0871686
oncletom <thomas@oncle-tom.net>
2012-04-28 21:37:08 +0200

    Minimal navigation box

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </head>
 <body <?php body_class() ?>>
 
-<header>
+<header id="header">
     <div class="container">
         <h1><a href="<?php echo home_url() ?>" rel="home"><?php bloginfo('title') ?></a></h1>
         <blockquote class="description"><?php bloginfo('description') ?></blockquote>
@@ -21,15 +21,37 @@
     <div class="row">
         <div class="span10" role="main">
             <div class="row">
-            <?php while(have_posts()): the_post() ?>
-            <div class="span5">
-                <article>
-                    <h2><a href="<?php the_permalink() ?>" title="<?php echo esc_attr(get_the_title()) ?>" rel="bookmark"><?php the_title() ?></a></h2>
+                <?php while(have_posts()): the_post() ?>
+                <div class="span5">
+                    <article>
+                        <h2><a href="<?php the_permalink() ?>" title="<?php echo esc_attr(get_the_title()) ?>" rel="bookmark"><?php the_title() ?></a></h2>
 
-                    <div class="excerpt"><?php the_excerpt() ?></div>
-                </article>
-            </div>
-            <?php endwhile ?>
+                        <div class="excerpt"><?php the_excerpt() ?></div>
+                    </article>
+                </div>
+                <?php endwhile ?>
+
+                <div class="span5 navigation" id="nav-links">
+                    <div class="article">
+                        <header>
+                            <?php posts_nav_link(' ', 'Articles plus récents', 'Articles plus anciens') ?>
+                        </header>
+
+                        <div class="content">
+                            <form action="/" method="get">
+                                <div class="input-append">
+                                    <input type="search" name="s" value="<?php the_search_query()?>" class="input-large" placeholder="..." required><button type="submit" class="btn">Chercher</button>
+                                </div>
+
+                            </form>
+
+                            <ul class="inline-list">
+                                <li class="heading">Sinon, les archives annuelles :</li>
+                                <?php wp_get_archives('type=yearly&show_post_count=true') ?>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
