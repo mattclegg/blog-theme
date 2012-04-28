fc6b54acc518b7bfe45cc9ea53a3d10093c25839
oncletom <thomas@oncle-tom.net>
2012-04-28 20:50:59 +0200

    Reworked header

--- index.php
+++ index.php
@@ -8,7 +8,11 @@
 <header>
     <div class="container">
         <h1><a href="<?php echo home_url() ?>" rel="home"><?php bloginfo('title') ?></a></h1>
-        <blockquote><?php bloginfo('description') ?></blockquote>
+        <blockquote class="description"><?php bloginfo('description') ?></blockquote>
+        <blockquote>{"location": "Bordeaux", "job": {
+            "CTO": "<a href="http://dijiwan.com">Dijiwan</a>",
+            "freelance": "<a href="http://cyneticmonkey.com">CyneticMonkey</a>"
+        }};</blockquote>
     </div>
 
 </header>
