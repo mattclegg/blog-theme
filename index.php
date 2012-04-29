67a4928faba326df49085e203022c23396d56fae
oncletom <thomas@oncle-tom.net>
2012-04-29 16:36:07 +0200

    Finalized navigation

--- index.php
+++ index.php
@@ -33,20 +33,19 @@
 
                 <div class="span5 navigation" id="nav-links">
                     <div class="article">
-                        <header>
-                            <?php posts_nav_link(' ', 'Articles plus récents', 'Articles plus anciens') ?>
-                        </header>
+                        <header>Et sinon ?</header>
 
                         <div class="content">
+                            <p class="previous-next"><?php posts_nav_link() ?></p>
+
                             <form action="/" method="get">
                                 <div class="input-append">
                                     <input type="search" name="s" value="<?php the_search_query()?>" class="input-large" placeholder="..." required><button type="submit" class="btn">Chercher</button>
                                 </div>
-
                             </form>
 
                             <ul class="inline-list">
-                                <li class="heading">Sinon, les archives annuelles :</li>
+                                <li class="heading">Ou enfin, les articles par année :</li>
                                 <?php wp_get_archives(array('type' => 'yearly')) ?>
                             </ul>
                         </div>
