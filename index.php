2359654b377da105817412869ddbe7b105b4b508
oncletom <thomas@oncle-tom.net>
2012-04-29 16:43:44 +0200

    Refactored loop navigation

--- index.php
+++ index.php
@@ -14,26 +14,7 @@
                 </div>
                 <?php endwhile ?>
 
-                <div class="span5 navigation" id="nav-links">
-                    <div class="article">
-                        <header>Et sinon ?</header>
-
-                        <div class="content">
-                            <p class="previous-next"><?php posts_nav_link() ?></p>
-
-                            <form action="/" method="get">
-                                <div class="input-append">
-                                    <input type="search" name="s" value="<?php the_search_query()?>" class="input-large" placeholder="..." required><button type="submit" class="btn">Chercher</button>
-                                </div>
-                            </form>
-
-                            <ul class="inline-list">
-                                <li class="heading">Ou enfin, les articles par année :</li>
-                                <?php wp_get_archives(array('type' => 'yearly')) ?>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                <?php get_template_part('loop', 'navigation') ?>
             </div>
         </div>
 
