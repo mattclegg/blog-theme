09d8c28bcf9289c0d90c73f6d464b04d58942357
Thomas Parisot <thomas@oncle-tom.net>
2013-05-04 00:54:09 +0200

    Updating hardcoded links

--- header.php
+++ header.php
@@ -5,14 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php wp_title() ?></title>
     <?php wp_head() ?>
-    <!-- Feeds -->
-    <meta name="verify-v1" content="gd91pL+4TgGMyfkxvZfbjTHlEXP92hn1s8syzjPTbUw=" />
-    <!--// Serveur OpenID //-->
-    <link rel="openid.server" href="http://www.myopenid.com/server" />
-    <link rel="openid.delegate" href="http://oncle.tom.myopenid.com/" />
-    <link rel="openid2.local_id" href="http://oncle.tom.myopenid.com" />
-    <link rel="openid2.provider" href="http://www.myopenid.com/server" />
-    <meta http-equiv="X-XRDS-Location" content="http://www.myopenid.com/xrds?username=oncle.tom.myopenid.com" />
 </head>
 <body <?php body_class() ?>>
 
@@ -25,11 +17,11 @@
                 <span class="icon-bar"></span>
             </a>
 
-            <a class="brand" href="https://case.oncle-tom.net/slash-me">@ Thomas Parisot</a>
+            <a class="brand" href="/slash-me">@ Thomas Parisot</a>
             <div class="nav-collapse collapse">
                 <nav class="nav">
                     <li><a href="http://cyneticmonkey.com/">Expertise Web</a></li>
-                    <li class="active"><a href="https://case.oncle-tom.net">Blog</a></li>
+                    <li class="active"><a href="<?php echo home_url() ?>">Blog</a></li>
                     <li><a href="http://cyneticmonkey.com/trainings">Formations</a></li>
                     <li><a href="http://cyneticmonkey.com/talks">Conférences</a></li>
                 </nav>
