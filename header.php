82aaae49f03c638e83040090f221c630b19f9366
Thomas Parisot <thomas@oncle-tom.net>
2013-05-04 10:28:49 +0200

    Fixing mobile device orientation issues

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes() ?>>
 <head>
     <meta http-equiv="Content-Type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="initial-scale=1.0" />
     <title><?php wp_title() ?></title>
     <?php wp_head() ?>
     <link rel="publisher" href="https://plus.google.com/113450102076209289116" />
