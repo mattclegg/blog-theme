8aab6e1ce8584221bda7422a7a18ec2be3f91672
Thomas Parisot <thomas@oncle-tom.net>
2013-05-04 10:28:26 +0200

    rel=author+publisher

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php wp_title() ?></title>
     <?php wp_head() ?>
+    <link rel="publisher" href="https://plus.google.com/113450102076209289116" />
+    <link rel="author" href="https://plus.google.com/113450102076209289116" />
 </head>
 <body <?php body_class() ?>>
 
