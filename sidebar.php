fca23d2e679044e24a2637e1b269e85d61a1a380
oncletom <thomas@oncle-tom.net>
2013-05-05 19:47:46 +0100

    Serving the avatar locally

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,7 @@
 <p class="about">
-    <a href="<?php echo site_url('/slash-me') ?>" rel="me" class="about"><?php echo get_avatar(get_option('admin_email'), 140) ?></a>
+    <a href="<?php echo site_url('/slash-me') ?>" rel="me" class="about">
+        <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/avatar.jpg" alt="" class="avatar">
+    </a>
 </p>
 
 <?php wp_nav_menu(array(
